e599f6173f507e9c26dd5da93867570ab8fdc13d
Teresa Pelinski <teresapelinski@gmail.com>
2020-04-09 16:20:53 +0200

    minor fix

--- bot.js
+++ bot.js
@@ -33,30 +33,6 @@ bot.onText(/\/word (.+)/, (msg, match) => { // only responses that start with /w
   });
 
 
-  bot.onText(/\/word (.+)/, (msg, match) => { // only responses that start with /word
-    const chatId = msg.chat.id;
-    const word = match[1];
-    axios
-      .get(`${process.env.OXFORD_API_URL}/entries/en-gb/${word}`, {
-        params: {
-          fields: 'definitions',
-          strictMatch: 'false'
-        },
-        headers: {
-          app_id: process.env.OXFORD_APP_ID,
-          app_key: process.env.OXFORD_APP_KEY
-        }
-      })
-      .then(response => {
-        const parsedHtml = parser(response.data);
-        bot.sendMessage(chatId, parsedHtml, { parse_mode: 'HTML' });
-      })
-      .catch(error => {
-        const errorText = error.response.status === 404 ? `No definition found for the word: <b>${word}</b>` : `<b>An error occured, please try again later</b>`;
-        bot.sendMessage(chatId, errorText, { parse_mode:'HTML'})
-      });
-  });
-
 
   bot.onText(/\/sum (.+)/, (msg, match) => { // only responses that start with /word
     const chatId = msg.chat.id;
